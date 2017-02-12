962355fc333fe6b03061bdb3fe0ab925df691c97
Jeremy Kithome <jeremy.kithome@andela.com>
2017-02-12 13:58:47 +0300

    Add todo editing functionality

---
+++ src/components/Todo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class='ui centered card'>
+    <div class='content'>
+      <div class='ui centered card' v-show="!isEditing">
+        <div class='content'>
+            <div class='header'>
+                {{ todo.title }}
+            </div>
+            <div class='meta'>
+                {{ todo.project }}
+            </div>
+            <div class='extra content'>
+                <span class='right floated edit icon' v-on:click="showForm">
+                <i class='edit icon'></i>
+              </span>
+            </div>
+        </div>
+        <div class='ui bottom attached green basic button' v-show="todo.done" disabled>
+            Completed
+        </div>
+        <div class='ui bottom attached red basic button' v-show="!todo.done">
+            Complete
+        </div>
+      </div>
+      <div class='ui form' v-show="isEditing">
+        <div class='field'>
+          <label>Title</label>
+          <input type='text' ref='title' v-model="todo.title" >
+        </div>
+        <div class='field'>
+          <label>Project</label>
+          <input type='text' ref='project' v-model="todo.title" >
+        </div>
+        <div class='ui two button attached buttons'>
+          <button class='ui basic blue button' v-on:click"hideForm">
+            Close X
+          </button>
+        </div>
+      </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props: ['todo'],
+    data() {
+      return {
+        isEditing: false,
+      };
+    },
+    methods: {
+      showForm() {
+        this.isEditing = true;
+      },
+      hideForm() {
+        this.isEditing = false;
+      },
+    },
+  };
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,32 +2,18 @@
   <div>
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <div class='ui centered card' v-for="todo in todos">
-      <div class='content'>
-        <div class='header'>
-          {{ todo.title }}
-        </div>
-        <div class='meta'>
-          {{ todo.project }}
-        </div>
-        <div class='extra content'>
-          <span class='right floated edit icon'>
-            <i class='edit icon'></i>
-          </span>
-        </div>
-      </div>
-      <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-      </div>
-      <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-      </div>
+    <todo  v-for="todo in todos" :todo.sync="todo"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
 
+import Todo from './Todo';
+
 export default {
   props: ['todos'],
+  components: {
+    Todo,
+  },
 };
 </script>
