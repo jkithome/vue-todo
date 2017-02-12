435d2ac0f16a841369f59e989badfabf1e2fb1f1
Jeremy Kithome <jeremy.kithome@andela.com>
2017-02-12 11:54:01 +0300

    Add data to main component and render todos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <todo-list></todo-list>
+    <div class='content'>
+      <todo-list v-bind:todos="todos"></todo-list>
+    </div>
   </div>
 </template>
 
@@ -11,6 +13,27 @@ export default {
   name: 'app',
   components: {
     TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <ul>
-      <li> Todo A </li>
-      <li> Todo B </li>
-      <li> Todo C </li>
-    </ul>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <div class='ui centered card' v-for="todo in todos">
+      <div class='content'>
+        <div class='header'>
+          {{ todo.title }}
+        </div>
+        <div class='meta'>
+          {{ todo.project }}
+        </div>
+        <div class='extra content'>
+          <span class='right floated edit icon'>
+            <i class='edit icon'></i>
+          </span>
+        </div>
+      </div>
+      <div class='ui bottom attached green basic button' v-show="todo.done">
+        Completed
+      </div>
+      <div class='ui bottom attached red basic button' v-show="!todo.done">
+        Complete
+      </div>
   </div>
 </template>
 
 <script type = "text/javascript" >
 
 export default {
+  props: ['todos'],
 };
 </script>
-<style>
-</style>
