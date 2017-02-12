693f532f709af5585f37bf6a1166484bc59cdea5
Jeremy Kithome <jeremy.kithome@andela.com>
2017-02-12 07:48:20 +0300

    Add a basic TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    TodoList,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      <li> Todo A </li>
+      <li> Todo B </li>
+      <li> Todo C </li>
+    </ul>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+
+export default {
+};
+</script>
+<style>
+</style>
